9afe32017a55e339b77bc5c0d74f9bf343c000aa
josef <Josef.Gabrielsson@goteborgsregionen.se>
2019-10-17 19:06:05 +0200

    Model Validation

--- Retail/Gunnebo/Data/OrderModel.cs
+++ Retail/Gunnebo/Data/OrderModel.cs
@@ -11,12 +11,16 @@ namespace gunnebo.Data
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long OrderId { get; private set; }
 
+        [Range(0, Int64.MaxValue, ErrorMessage = "{0} invalid value.")]
         public long OrderNumber { get; set; }
 
+        [Range(0, Int64.MaxValue, ErrorMessage = "{0} invalid value.")]
         public long OrderRegistrationNumber { get; set; }
 
+        [EnumDataType(typeof(OrderStatusEnum), ErrorMessage = "{0} invalid status.")]
         public OrderStatusEnum OrderStatus { get; set; } // Enum value, TODO: OrderStatus is public 
 
+        [DataType(DataType.Date)]
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public DateTime OrderDate { get; private set; }
     }
