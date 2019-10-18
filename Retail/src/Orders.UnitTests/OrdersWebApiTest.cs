6b8bc0bc6bf4b5ed11cf1a24ac25de2fbe56402a
josef <Josef.Gabrielsson@goteborgsregionen.se>
2019-10-18 02:10:50 +0200

    Major updates

---
+++ Retail/src/Orders.UnitTests/OrdersWebApiTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Orders.UnitTests
+{
+    public class OrdersWebApiTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
