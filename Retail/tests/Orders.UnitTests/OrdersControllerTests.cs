c74073fbcc2f294c25f84c992df7e09218b9e32c
josef <Josef.Gabrielsson@goteborgsregionen.se>
2019-10-21 01:15:00 +0200

    Name change

---
+++ Retail/tests/Orders.UnitTests/OrdersControllerTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Moq;
+
+namespace Orders.UnitTests
+{
+    public class OrdersControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
