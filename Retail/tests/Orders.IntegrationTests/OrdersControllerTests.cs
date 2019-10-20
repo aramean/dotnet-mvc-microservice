2be05b814bf73716f2f055eef1fbc4417f49d734
josef <Josef.Gabrielsson@goteborgsregionen.se>
2019-10-21 01:09:22 +0200

    More integration test

--- Retail/tests/Orders.IntegrationTests/OrdersControllerTests.cs
+++ Retail/tests/Orders.IntegrationTests/OrdersControllerTests.cs
@@ -6,7 +6,9 @@ using System.IO;
 using System.Net;
 using System.Net.Http;
 using FluentAssertions;
+using FluentAssertions.Extensions;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Orders.IntegrationTests
 {
@@ -29,8 +31,79 @@ namespace Orders.IntegrationTests
             response.Content.Headers.ContentType.ToString().Should().Be("application/json; charset=utf-8");
         }
 
+        [Theory]
+        [InlineData("api/orders/0")]
+        public async Task Get_ShouldBeOKAndCorrectContentType(string url)
+        {
+
+            // Arange
+            //await AuthenticateAsync();
+
+            // Act
+            var response = await TestClient.GetAsync(url);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.Content.Headers.ContentType.ToString().Should().Be("application/json; charset=utf-8");
+        }
+
+        [Theory]
+        [InlineData("api/orders/0")]
+        public async Task Put_ShouldBeOKAndCorrectContentType(string url)
+        {
+
+            // Arange
+            //await AuthenticateAsync();
+            var payload = "{\"OrderId\": 0, \"OrderNumber\": 0, \"OrderRegistrationNumber\": 0}";
+            var content = new StringContent(payload, Encoding.UTF8, "application/json");
+
+            // Act
+            var response = await TestClient.PutAsync(url, content);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.Content.Headers.ContentType.ToString().Should().Be("application/json; charset=utf-8");
+        }
+
+        [Theory]
+        [InlineData("api/orders/")]
+        public async Task Post_ShouldBeOKWithRespondNewAndCorrectContentType(string url)
+        {
+
+            // Arange
+            //await AuthenticateAsync();
+            var payload = "{\"OrderNumber\": 1, \"OrderRegistrationNumber\": 1}";
+            var content = new StringContent(payload, Encoding.UTF8, "application/json");
+
+            // Act
+            var response = await TestClient.PostAsync(url, content);
+            var contents = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Created);
+            response.Content.Headers.ContentType.ToString().Should().Be("application/json; charset=utf-8");
+            contents.Should().Contain("\"orderStatus\":\"New\"");
+        }
+
+
+        [Theory]
+        [InlineData("api/orders/")]
+        public async Task Post_ShouldBeConflictAndCorrectContentType(string url)
+        {
+
+            // Arange
+            //await AuthenticateAsync();
+            var payload = "{\"OrderNumber\": 0, \"OrderRegistrationNumber\": 0}";
+            var content = new StringContent(payload, Encoding.UTF8, "application/json");
+
+            // Act
+            var response = await TestClient.PostAsync(url, content);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Conflict);
+            response.Content.Headers.ContentType.ToString().Should().Be("application/problem+json; charset=utf-8");
+        }
+
     }
 
 }
-
-
