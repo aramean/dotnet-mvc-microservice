0fe35594c3a2a4f8ea386308174058226c819fdd
josef <Josef.Gabrielsson@goteborgsregionen.se>
2019-10-25 01:20:45 +0200

    Docker fix

--- Retail/src/Startup.cs
+++ Retail/src/Startup.cs
@@ -21,7 +21,7 @@ namespace Orders
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<OrderContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<OrderContext>(options => options.UseSqlServer(Configuration["ConnectionString"]));
             //services.AddDbContext<OrderContext>(options => options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
             //services.AddDbContext<OrderContext>(opt => opt.UseInMemoryDatabase("OrderList"));
 
